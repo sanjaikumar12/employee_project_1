f431e522df3c14f7c74081d59fca3677456a3aaf
sanjaikumar12 <52711189+sanjaikumar12@users.noreply.github.com>
2024-06-23 19:59:08 +0530

    Update EmployeeService.java

--- src/main/java/com/hendisantika/springbootthymeleafemployeecrud/service/EmployeeService.java
+++ src/main/java/com/hendisantika/springbootthymeleafemployeecrud/service/EmployeeService.java
@@ -5,15 +5,7 @@ import org.springframework.data.domain.Page;
 
 import java.util.List;
 
-/**
- * Created by IntelliJ IDEA.
- * Project : springboot-thymeleaf-employee-crud
- * User: hendisantika
- * Email: hendisantika@gmail.com
- * Telegram : @hendisantika34
- * Date: 19/10/20
- * Time: 21.58
- */
+
 public interface EmployeeService {
     List<Employee> getAllEmployees();
 
