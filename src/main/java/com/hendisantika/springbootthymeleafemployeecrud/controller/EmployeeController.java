e6078d717e0a6b51b45f9967faceac542b2e18bf
sanjaikumar12 <52711189+sanjaikumar12@users.noreply.github.com>
2024-06-23 20:00:59 +0530

    Update EmployeeController.java

--- src/main/java/com/hendisantika/springbootthymeleafemployeecrud/controller/EmployeeController.java
+++ src/main/java/com/hendisantika/springbootthymeleafemployeecrud/controller/EmployeeController.java
@@ -14,15 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
-/**
- * Created by IntelliJ IDEA.
- * Project : springboot-thymeleaf-employee-crud
- * User: hendisantika
- * Email: hendisantika@gmail.com
- * Telegram : @hendisantika34
- * Date: 19/10/20
- * Time: 22.01
- */
+
 @Controller
 public class EmployeeController {
 
